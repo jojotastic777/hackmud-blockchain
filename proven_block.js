ce167879b6ac0a87cfe6d7f95ea11fda0745dc07
jojotastic777 <jojotastic777@users.noreply.github.com>
2017-07-18 12:59:01 -0700

    Update proven_block.js
    Added in-file documentation.

--- proven_block.js
+++ proven_block.js
@@ -2,9 +2,17 @@ function (context, args) {
   //This is an add-on module for my block chain library, adding a generic "proof-of-work" block, with adjustable difficulty.
   //Be careful with the difficulty, however as setting it too high will, as of this initial release, render the block unsolvable
   //in the amount of time hackmud allows. I will likely implement db-based solving over multiple runs of the script.
+  
   var bcl =    #s.kvothe_the_arcane.blockchainlib()
   var cryptoLib = #s.kvothe_the_arcane.cryptolib()
 
+  /**
+   * GenericProvenBlock: GenericProvenBlock is like the GenericBlock from blockChainLib in almost every way, except that this runs a proof-of-work algorithm, while GenericBlock does not.
+   * @param       {array} blockChain  Documented in blockChainLib.
+   * @param       {number} difficulty Sets the difficulty of the proof-of-work algorithm. Be careful, as setting the difficulty too high will make it impossible to solve within hackmud's time limits. I will likely implement a way to solve over multiple runs in the future.
+   * @param       {object} data       The property "data" is set to this value. This is the property that usually changes between block types.
+   * @constructor
+   */
   function GenericProvenBlock (blockChain, difficulty, data) {
     let genericProvenBlock = {
       index: blockChain.length,
